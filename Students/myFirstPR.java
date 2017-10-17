e1e424d36a2cc148c7947251f43dc98ac901f261
Dimitar Gadzhev <dimitar.gadzhev@mentormate.com>
2017-10-17 13:52:23 +0300

    My first commit by Dimitar Gadzhev

---
+++ Students/myFirstPR.java
@@ -0,0 +1,13 @@
+public class myFirstPR 
+{
+    private String message;
+    
+    public myFirstPR() {
+        message = "Hello, this is my first commit"; 
+    }
+
+    public void sayHello() {
+        System.out.println(message);
+    }
+
+}

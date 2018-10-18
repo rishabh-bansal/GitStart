fa6c15f6b5d4a929504cbd58fcbbd55bad14943f
[LakinduK] <[lakindu1997@gmail.com]>
2018-10-18 13:48:43 +0530

    simple program to find max in C language

---
+++ Students/FindMaximum_C_Language.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,max;
+	printf("enter 2 values to find the maximum:");
+	 scanf("%d %d",&a,&b);
+	   if(a<b)
+	     b=max;
+	   else
+	     a=max;
+	
+	printf("maximum is:",max);
+}

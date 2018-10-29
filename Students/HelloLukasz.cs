77294dc2e4bed779e5261217f59b34a7c4aca25e
Łukasz Kubasiewicz <lukasz@mycs.com>
2018-10-29 10:49:18 +0100

    HelloLukasz.cs

---
+++ Students/HelloLukasz.cs
@@ -0,0 +1,12 @@
+using System;
+ namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Witajcie! Pozdrowienia z Polski");
+            Console.ReadKey();
+        }
+    }
+}

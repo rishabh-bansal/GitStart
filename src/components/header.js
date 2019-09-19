bf2b44f73c4baefe735fd2a64313795721e3c9fb
Rishabh Bansal <rishabhbansal1855@gmail.com>
2019-09-19 14:44:34 +0530

    Udpate Link

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = ({ siteTitle }) => (
       <div className="nav">
           <div className="brand-text">{ siteTitle }</div>
           <div className="github-logo">
-              <a target="_blank" href="https://github.com/haxzie/GitMe">
+              <a target="_blank" href="https://github.com/rishabh-bansal/GitStart">
                   <img alt="github-logo" src={github_logo}/>
               </a>
           </div>

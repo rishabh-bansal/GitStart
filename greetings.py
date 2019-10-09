d7847fbc1252335307b632e6284f25360a56e44c
varunjcnayak <42367164+varunjcnayak@users.noreply.github.com>
2019-10-09 20:02:54 +0530

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+name=raw_input("Enter you name: ")
+from random import randint
+i=randint(1,4)
+
+if (i==1):
+	print "Hello",name
+elif (i==2):
+	print "G'day",name
+elif (i==3):
+	print "Greetings,",name
+else:
+	print "Hi",name 
+
